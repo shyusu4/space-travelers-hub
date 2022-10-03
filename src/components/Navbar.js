40e38eeb27db70fd1ecb4d714828cba7573a690e
unknown <shyusupova4@gmail.com>
2022-10-03 19:42:53 +0500

    Add navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,16 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
-      <Link to="/"></Link>
+      <div className='logo-container'>
+        <img src={require('./img/planet.png')} alt='logo'></img>
+      </div>
+      <h1>Space Travelers' Hub</h1>
+      <ul className='links-container'>
+        <li><Link to="/">Rockets</Link></li>
+        <li><Link to="/">Missions</Link></li>
+        <hr />
+        <li><Link to="/">My Profile</Link></li>
+      </ul>
     </nav>
   );
 }
