3ce1e7ba9414995a490611e33233d250f7260448
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-10-06 23:25:34 +0300

    Update Myprofile.js

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,32 +1,41 @@
 import { useSelector } from 'react-redux';
-
+import '../App.css';
 function MyProfile() {
   const missions = useSelector((state) => state.missionsReducer.missions);
   const joinedMissions = missions.filter((mission) => mission.reserved === true);
   const rockets = useSelector((state) => state.rocketsReducer);
-  const filterRockets = rockets.rockets.filter((rocket) => rocket.reserved == true);
+  const filterRockets = rockets.rockets.filter((rocket) => rocket.reserved === true);
   return (
     <div className="my-profile">
       <table>
-        <thead>
-          <tr>
-            <th>My Missions</th>
-          </tr>
-        </thead>
-        <tbody className="my-missions">
-          {joinedMissions.map((mission) => (
-            <tr key={mission.id}>
-              <td>{mission.name}</td>
+        <div>
+          <thead>
+            <tr>
+              <th>My Missions</th>
             </tr>
-          ))}
-        </tbody>
-        <tbody className="my-rockets">
-          {filterRockets.length === 0 ? (
-            <p>Let get some rockets !!!!</p>
-          ) : (
-            filterRockets.map((rocket) => <p key={rocket.id}>{rocket.rocket_name}</p>)
-          )}
-        </tbody>
+          </thead>
+          <tbody className="my-missions">
+            {joinedMissions.map((mission) => (
+              <tr key={mission.id}>
+                <td>{mission.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </div>
+        <div className="rocketsCon">
+          <th className="rocket-title">My Rockets</th>
+          <tbody className="my-rockets">
+            {filterRockets.length === 0 ? (
+              <p className="rocketp">Let get some rockets !!!!</p>
+            ) : (
+              filterRockets.map((rocket) => (
+                <p key={rocket.id} className="rocket-list">
+                  {rocket.rocket_name}
+                </p>
+              ))
+            )}
+          </tbody>
+        </div>
       </table>
     </div>
   );
