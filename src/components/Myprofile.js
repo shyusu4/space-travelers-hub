e8783580c59f10a48dee07d085ef7d2a31585722
unknown <shyusupova4@gmail.com>
2022-10-06 14:26:05 +0500

    Add missions to my profile

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,8 +1,26 @@
+import { useSelector } from 'react-redux';
+
 function MyProfile() {
+  const missions = useSelector((state) => state.missionsReducer.missions);
+  const joinedMissions = missions.filter((mission) => mission.reserved === true);
+
   return (
-    <>
-      <h2>My Profile</h2>
-    </>
+    <div className='my-profile'>
+      <table>
+        <thead>
+        <tr>
+          <th>My Missions</th>
+        </tr>
+        </thead>
+        <tbody className="my-missions">
+            {joinedMissions.map((mission) => (
+              <tr key={mission.id}>
+                <td>{mission.name}</td>
+                </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
